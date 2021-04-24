0293dd9881e875badb053d0ab67670851b69d47e
bayusetiawan01 <61371193+bayusetiawan01@users.noreply.github.com>
2021-04-24 09:03:22 +0700

    Kompeten

--- frontend/views.py
+++ frontend/views.py
@@ -21,12 +21,12 @@ def strukturOrganisasi(request, *args, **kwargs):
 
 def mainCategories(request, maincat, *args, **kwargs):
     if maincat == "home":
-        return render(request, 'frontend/home.html')
+        return render(request, 'frontend/home.html', {'cat': maincat})
     elif maincat == "about":
-        return render(request, 'frontend/about.html')
+        return render(request, 'frontend/about.html', {'cat': maincat})
     elif maincat == "blog":
-        return render(request, 'frontend/blog.html')
+        return render(request, 'frontend/blog.html', {'cat': maincat})
     elif maincat == "enclass":
-        return render(request, 'frontend/enclass.html')
+        return render(request, 'frontend/enclass.html', {'cat': maincat})
     elif maincat == "kompeten":
-        return render(request, 'frontend/kompeten.html')
+        return render(request, 'frontend/kompeten.html', {'cat': maincat})
